d442bbbae804ce831771cc3060ffb4f5d37e2bd9
Alejandro Estrella <91709069+EpicPizza@users.noreply.github.com>
2022-09-14 17:42:42 -0700

    Update RobotContainer.java

--- CommandBasedChallenge/src/main/java/frc/robot/RobotContainer.java
+++ CommandBasedChallenge/src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.DriveForwardTimed;
 import frc.robot.commands.DrivePID;
+import frc.robot.commands.IntakeTimed;
+import frc.robot.commands.ShooterDistance;
+import frc.robot.commands.ShooterTimed;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
@@ -25,6 +30,11 @@ public class RobotContainer {
   private final DriveTrain driveTrain;
   private final DriveForwardTimed driveForwardTimed;
   private final DrivePID drivePID;
+  private final Shooter shooter;
+  private final ShooterDistance shootDistance;
+  private final ShooterTimed shootTimed;
+  private final Intake intake;
+  private final IntakeTimed intakeTimed;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -36,6 +46,20 @@ public class RobotContainer {
 
     drivePID = new DrivePID(driveTrain);
     drivePID.addRequirements(driveTrain);
+    
+    shooter = new Shooter();
+    
+    shootDistance = new ShooterDistance(shooter);
+    shootDistance.addRequirements(shooter);
+
+    shootTimed = new ShooterTimed(shooter);
+    shootTimed.addRequirements(shooter);
+
+    intake = new Intake();
+    intakeTimed = new IntakeTimed(intake);
+    intakeTimed.addRequirements(intake);
+    intake.setDefaultCommand(intakeTimed);
+    
     configureButtonBindings();
   }
 
